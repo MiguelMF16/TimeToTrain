e2c8e5f0ad3caeee3ed7c9a20bab9111ce9f4a00
MiguelMartin <martinfopiani@gmail.com>
2020-03-03 23:48:23 +0100

    Nuevo layout lolestadisticas, nuevo boton en traininglol, nuevo background personalizado para lolestadisticas.

---
+++ app/src/main/java/com/example/timetotrain/lol_estadisticas.java
@@ -0,0 +1,19 @@
+package com.example.timetotrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class lol_estadisticas extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lol_estadisticas);
+
+
+    }
+}
